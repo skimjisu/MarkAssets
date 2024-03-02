f77fbdf2db0f85dda26b7f5ec116a8085dfe2c30
skimjisu@naver.com <skimjisu>
2024-03-02 21:45:26 +0900

    commit

---
+++ Loginfrm.pas
@@ -0,0 +1,24 @@
+unit Loginfrm;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TForm9 = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form9: TForm9;
+
+implementation
+
+{$R *.dfm}
+
+end.
