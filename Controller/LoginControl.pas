2281f11dba86ff347d72338300314e0022ec2db9
skimjisu@naver.com <skimjisu>
2024-03-03 00:37:52 +0900

    코드 변경

--- Controller/LoginControl.pas
+++ Controller/LoginControl.pas
@@ -3,21 +3,30 @@ unit LoginControl;
 interface
 
 uses
-  System.SysUtils, Vcl.Dialogs, DBProce, Data.DB, FireDAC.Comp.Client, FireDAC.Stan.Def, FireDAC.DApt, FireDAC.Comp.UI, FireDAC.Stan.Async;
+  System.SysUtils, FireDAC.Comp.Client, FireDAC.Phys.SQLite, FireDAC.Phys.SQLiteDef,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf,
+  FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async,
+  FireDAC.Phys, FireDAC.VCLUI.Wait, Data.DB, DBProce, FireDAC.DApt, FireDAC.Comp.UI, windows, Vcl.Dialogs;
+
 
 type
+  TLoginEvent = procedure(Sender: TObject; Success: Boolean) of object;
+
   TLoginControl = class
   private
+    FOnLogin: TLoginEvent;
     FUsername: string;
     FPassword: string;
     FAuthorityLevel: string;
     FConnection: TFDConnection;
     function IsAuthenticated: Boolean;
+
   public
     constructor Create(const Username, Password: string);
     destructor Destroy; override;
     procedure CreateAccount;
     procedure Login;
+    property OnLogin: TLoginEvent read FOnLogin write FOnLogin;
   end;
 
 implementation
@@ -79,20 +88,28 @@ var
   WaitCursor: TFDGUIxWaitCursor;
   Success: Boolean;
 begin
-  WaitCursor := TFDGUIxWaitCursor.Create(nil);   // WaitCursor 생성
+  // WaitCursor 생성
+  WaitCursor := TFDGUIxWaitCursor.Create(nil);
   try
     WaitCursor.Provider := 'Forms';  // Provider 설정
 
     // 로그인 처리
     try
+      // IsAuthenticated 메서드는 사용자가 올바른 인증 정보를 제공했는지 확인합니다.
+      // 이 메서드는 TLoginControl 클래스에 정의되어 있어야 합니다.
       Success := IsAuthenticated;
 
       if Success then
       begin
-        ShowMessage('로그인 성공!')
+        // OnLogin 이벤트를 트리거합니다.
+        // 이 이벤트는 로그인 성공 시에 호출되어야 하는 동작을 정의합니다.
+        if Assigned(FOnLogin) then
+          FOnLogin(Self, Success);
       end
       else
       begin
+        // CreateAccount 메서드는 새 계정을 생성합니다.
+        // 이 메서드는 TLoginControl 클래스에 정의되어 있어야 합니다.
         CreateAccount;
         ShowMessage('계정이 생성되었습니다. 다시 로그인 해주세요.');
       end;
@@ -102,7 +119,8 @@ begin
     end;
 
   finally
-    WaitCursor.Free;  // WaitCursor 제거
+    // WaitCursor 제거
+    WaitCursor.Free;
   end;
 end;
 
